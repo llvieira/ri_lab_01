bfdf6cc1b053511334d9c1f8f6ab8da788c2d847
Lucas Vieira <lima.lucaslv@gmail.com>
2019-04-02 15:40:37 -0300

    Implementing parse

--- ri_lab_01/spiders/diario_do_centro_do_mundo.py
+++ ri_lab_01/spiders/diario_do_centro_do_mundo.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import re
+import pdb
 
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
@@ -18,14 +20,18 @@ class DiarioDoCentroDoMundoSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-        #
-        #
-        #
+        response = response.css('div.td_with_ajax_pagination')[0].css('script *::text').get()
+        td_atts = json.loads(re.findall("{.*}", response)[0])
+        td_atts["limit"] = 20
+
+        body = {"td_atts": td_atts, "td_block_id": "td_uid_2_5ca3920f6fbd3", "td_column_number": "3",
+                "td_current_page": "1", "block_type": "td_block_4", "td_filter_value": "",
+                "td_user_action": "", "action": "td_ajax_block"}
+
+        yield scrapy.FormRequest("https://www.diariodocentrodomundo.com.br/wp-admin/admin-ajax.php",
+                                 callback=self.news_parse, formdata=body)
+
+    def news_parse(self, response):
+        print(response.url)
+        pdb.set_trace()
+
