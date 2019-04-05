145044abe9fa36481a8c9795156674186d5b0cdb
Lucas Vieira <lima.lucaslv@gmail.com>
2019-04-05 00:00:07 -0300

    Remove donation paragraph

--- ri_lab_01/spiders/diario_do_centro_do_mundo.py
+++ ri_lab_01/spiders/diario_do_centro_do_mundo.py
@@ -24,8 +24,12 @@ class DiarioDoCentroDoMundoSpider(scrapy.Spider):
     In this method, we get the first news of any of the pages in
     seeds/diario_do_centro_do_mundo.json file.
     
-    With the first news in hand, we can scrape it to get other different news.
-    We can accomplish this by using the parse_news callback. 
+    With the first news in hand, we can scrape it to get other different news
+    from the "Artigo anterior" reference. We can accomplish this by using the 
+    parse_news callback. 
+    
+    Since the website have pagination, the crawler could only get the last 5 
+    news, which is why we chose this different approach.
     """
     def parse(self, response):
         news_url = response.css('h3.td-module-title a::attr(href)').get()
@@ -67,21 +71,31 @@ class DiarioDoCentroDoMundoSpider(scrapy.Spider):
     stored on the output/results.csv file.
     """
     def create_news_item(self, response):
-        url = response.url
-        title = response.css('header h1::text').get()
-        author = response.css('div.td-post-author-name a::text').get()
         date = response.css('span.td-post-date time::attr(datetime)').get()
-        date = self.format_date(date)
-        category = response.meta.get('category')
-        text = response.css('div.td-post-content span.s1::text').getall()
+        news_date = self.get_datetime(date)
+        threshold_date = datetime.datetime(2018, 1, 1)
 
-        # trying to get the news text from a different tag.
-        if not text:
-            text = response.css('div.td-post-content p::text').getall()
+        # check if the news is up to date.
+        if news_date > threshold_date:
+            url = response.url
+            news_date = self.format_date(news_date)
+            title = response.css('header h1::text').get()
+            author = response.css('div.td-post-author-name a::text').get()
+            category = response.meta.get('category')
+            text = response.css('div.td-post-content span.s1::text').getall()
 
-        # NOTE: we don't have the sub_title information in the news.
-        return RiLab01Item(title=title, author=author, date=date,
-                           text=text, url=url, section=category)
+            # trying to get the news text from a different tag.
+            if not text:
+                text = response.css('div.td-post-content p::text').getall()
+                donation_paragraph = response.css('p.donation_paragraph::text').get()
+
+                if text[-1] == donation_paragraph:
+                    # remove denotation paragraph.
+                    del text[-1]
+
+            # NOTE: we don't have the sub_title information in the news.
+            return RiLab01Item(title=title, author=author, date=news_date,
+                               text=text, url=url, section=category)
 
     """
     Get tne news category based on its URL.
@@ -90,13 +104,18 @@ class DiarioDoCentroDoMundoSpider(scrapy.Spider):
         return url.split('/')[3].capitalize()
 
     """
+    Convert date string to datetime Python object.
+    """
+    def get_datetime(self, news_date):
+        news_date = news_date.split("T")
+        period = news_date[0]
+        time = news_date[1].split("+")[0]
+
+        news_date = period + " " + time
+        return datetime.datetime.strptime(news_date, '%Y-%m-%d %H:%M:%S')
+
+    """
     Format date to follow the Latin America date format.
     """
-    def format_date(self, date):
-        date = date.split("T")
-        period = date[0]
-        time = date[1].split("+")[0]
-
-        date = period + " " + time
-        obj = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-        return obj.strftime("%d/%b/%Y %H:%M:%S")
+    def format_date(self, news_datetime):
+        return news_datetime.strftime("%d/%b/%Y %H:%M:%S")
